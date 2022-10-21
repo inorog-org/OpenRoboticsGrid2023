3c148cc54642bbc6112ad6895877773568b23187
Robert Rachieru <Pawnchy.Swine52@gmail.com>
2022-10-21 21:14:59 +0300

    OpMode Cuirasat

---
+++ TeamCode/src/main/java/opmodes/Cuirasat.java
@@ -0,0 +1,11 @@
+package opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Cuirasat extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
