bfad365370194e9607a0e72eacebc45ba457873d
Hatmanul-RRC <Pawnchy.Swine52@gmail.com>
2022-11-20 19:44:56 +0200

    Update

--- TeamCode/src/main/java/opmodes/Cuirasat.java
+++ TeamCode/src/main/java/opmodes/Cuirasat.java
@@ -1,11 +1,20 @@
 package opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name = "Concept: Mecanum Lunatica", group = "Concept")
 public class Cuirasat extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+
+        }
 
     }
 }
