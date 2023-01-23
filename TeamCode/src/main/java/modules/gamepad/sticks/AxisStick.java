8bdce0e4f2318ad496b81c1505a7785119115ab0
Hatmanul-RRC <Pawnchy.Swine52@gmail.com>
2023-01-23 19:06:50 +0200

    Gamepad Second Node - Stick

--- TeamCode/src/main/java/modules/gamepad/sticks/AxisStick.java
+++ TeamCode/src/main/java/modules/gamepad/sticks/AxisStick.java
@@ -1,10 +1,28 @@
 package modules.gamepad.sticks;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
 public class AxisStick implements Sticks {
 
+  private final Supplier<Double> stick;
+  private final BiFunction<Double,Double,Double> equation;
+  private final double DRIFT;
 
+  public AxisStick(Supplier<Double> stick, BiFunction<Double, Double, Double> equation, double drift) {
 
+      this.stick    = stick;
+      this.equation = equation;
+      this.DRIFT    = drift;
+  }
 
-
+  @RequiresApi(api = Build.VERSION_CODES.N)
+  public double getAxis() {
+      return equation.apply(stick.get(), DRIFT);
+  }
 
 }
---
+++ TeamCode/src/main/java/modules/gamepad/sticks/StickEquations.java
@@ -0,0 +1,31 @@
+package modules.gamepad.sticks;
+
+import java.util.function.BiFunction;
+
+public class StickEquations {
+
+    // -- Ecuatii pentru DRIFT -- //
+
+    /*   FROM ZERO - STICK
+
+    -1        0   0        1   - REMAPED STICK AFTER FROM_ZERO EQUATION - [-1,1]
+     |--------|-|-|--------|   - STICK AREA
+    -1       -D 0 D        1   - NORMAL STICK WITH DRIFT (D = drift)
+
+    */
+    public static final BiFunction<Double,Double,Double> FROM_ZERO  = (x, drift) -> drift;
+
+    /*   FROM DRIFT - STICK
+
+    -1       -D   D        1   - REMAPED STICK AFTER FROM_DRIFT EQUATION - [-1,1]\[-D,D]
+     |--------|-|-|--------|   - STICK AREA
+    -1       -D 0 D        1   - NORMAL STICK WITH DRIFT (D = drift)
+
+    */
+    public static final BiFunction<Double,Double,Double> FROM_DRIFT = (x, drift) -> drift;
+
+    // -- Ecuatii pentru Polar Stick -- //
+
+
+
+}
