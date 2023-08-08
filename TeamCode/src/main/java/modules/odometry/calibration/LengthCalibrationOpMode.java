ea447d7fc7859e5c925beba2297e10a0ccaff13a
Rachieru Robert <Pawnchy.Swin52@gmail.com>
2023-08-08 21:02:02 +0300

    Calibration OpModes to be done!!!

---
+++ TeamCode/src/main/java/modules/odometry/calibration/LengthCalibrationOpMode.java
@@ -0,0 +1,10 @@
+package modules.odometry.calibration;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class LengthCalibrationOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
