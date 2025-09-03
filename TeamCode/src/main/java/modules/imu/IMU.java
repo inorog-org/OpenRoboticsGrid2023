62d59d1aef1f3afe33bd0b4e0f505606dc132baa
n41ilexz <stamatenarcis20091501@gmail.com>
2025-09-03 09:48:35 +0300

    imu test

--- TeamCode/src/main/java/modules/imu/IMU.java
+++ TeamCode/src/main/java/modules/imu/IMU.java
@@ -1,20 +1,11 @@
 package modules.imu;
 
-import static org.firstinspires.ftc.robotcore.external.navigation.NavUtil.meanIntegrate;
-import static org.firstinspires.ftc.robotcore.external.navigation.NavUtil.plus;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-
 import com.qualcomm.hardware.bosch.BNO055IMUImpl;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.hardware.bosch.NaiveAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.RobotLog;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
@@ -56,8 +47,7 @@ public class IMU extends BNO055IMUImpl implements IMUInterface, IMURemapAxis, He
 
         parametersInit(angleUnit, accelUnit);
 
-       // this.setSensorMode(SensorMode.ACCONLY);
-
+        // facem imediat remapare pe IMU mode
         remapAxis(DEFAULT_AXIS_MAP_BYTE, DEFAULT_SIGN_MAP_BYTE);
     }
 
@@ -66,8 +56,8 @@ public class IMU extends BNO055IMUImpl implements IMUInterface, IMURemapAxis, He
 
         Parameters parameters = new Parameters();
 
-        // Sensor Mode
-        parameters.mode = SensorMode.NDOF; // Sau cu NDOF dacă vrem o calibrare mai bună cu Magnetometrul
+        // 🔹 Schimbat: folosim IMU Mode (fără magnetometru, mai stabil în FTC)
+        parameters.mode = SensorMode.IMU;
 
         // Units
         parameters.temperatureUnit = TempUnit.CELSIUS;
@@ -84,17 +74,16 @@ public class IMU extends BNO055IMUImpl implements IMUInterface, IMURemapAxis, He
         parameters.gyroBandwidth = GyroBandwidth.HZ32;
         parameters.gyroPowerMode = GyroPowerMode.NORMAL;
 
-        // Magnetometer Parameters
+        // Magnetometer Parameters (nu contează în IMU Mode)
         parameters.magRate = MagRate.HZ30;
         parameters.magOpMode = MagOpMode.REGULAR;
         parameters.magPowerMode = MagPowerMode.NORMAL;
 
         // Calibration File
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // Fișier cu calibrarea
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
 
-        // Algoritm
-       //parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-       parameters.accelerationIntegrationAlgorithm = new NaiveAccelerationIntegratorMAX();
+        // Algoritm integrare accel
+        parameters.accelerationIntegrationAlgorithm = new NaiveAccelerationIntegratorMAX();
 
         // Logging
         parameters.loggingEnabled = true;
@@ -105,30 +94,25 @@ public class IMU extends BNO055IMUImpl implements IMUInterface, IMURemapAxis, He
 
     // Setters
     public void setAxesOrder(AxesOrder axesOrder) {
-
         this.axesOrder = axesOrder;
     }
 
     public void setAxesRefrence(AxesReference axesReference) {
-
         this.axesReference = axesReference;
     }
 
     // Getters
     public Orientation getAngularOrientationDefault() {
-
         return getAngularOrientation(axesReference, axesOrder, angleUnit.toAngleUnit());
     }
 
     @Override
     public String getDeviceName() {
-
         return "BNO055";
     }
 
     @Override
     public Manufacturer getManufacturer() {
-
         return Manufacturer.Other;
     }
 
@@ -136,7 +120,6 @@ public class IMU extends BNO055IMUImpl implements IMUInterface, IMURemapAxis, He
     public void remapAxis(@NonNull Axis xAxis, @NonNull Axis yAxis, @NonNull Axis zAxis, @NonNull Sign xSign, @NonNull Sign ySign, @NonNull Sign zSign) {
 
         byte AXIS_MAP_CONFIG_BYTE = (byte) (xAxis.value | (yAxis.value << 2) | (zAxis.value << 4));
-
         byte AXIS_MAP_SIGN_BYTE = (byte) (xSign.sign | (ySign.sign << 1) | (zSign.sign << 2));
 
         remapAxis(AXIS_MAP_CONFIG_BYTE, AXIS_MAP_SIGN_BYTE);
@@ -146,18 +129,16 @@ public class IMU extends BNO055IMUImpl implements IMUInterface, IMURemapAxis, He
 
         // Setăm senzorul pe Configuration Mode
         write8(BNO055IMU.Register.OPR_MODE, BNO055IMU.SensorMode.CONFIG.bVal & 0x0F);
-
         opMode.sleep(100);
 
-        // Modificăm registrul AXIS_MAP_CONFIG cu configurația dorită
+        // Modificăm AXIS_MAP_CONFIG
         write8(BNO055IMU.Register.AXIS_MAP_CONFIG, AXIS_MAP_CONFIG_BYTE);
 
-        // Modificăm registrul AXIS_MAP_SIGN cu configurația dorită
+        // Modificăm AXIS_MAP_SIGN
         write8(BNO055IMU.Register.AXIS_MAP_SIGN, AXIS_MAP_SIGN_BYTE);
 
-        // Setăm Senzorul pe IMU Mode
+        // Setăm înapoi pe IMU Mode (consistent cu initialize)
         write8(BNO055IMU.Register.OPR_MODE, BNO055IMU.SensorMode.IMU.bVal & 0x0F);
-
         opMode.sleep(100);
     }
 
@@ -168,12 +149,10 @@ public class IMU extends BNO055IMUImpl implements IMUInterface, IMURemapAxis, He
 
     @Override
     public void initPositionTracker(Position initalPosition, Velocity initialVelocity, int msPollInterval) {
-          startAccelerationIntegration(initalPosition, initialVelocity, msPollInterval);
+        startAccelerationIntegration(initalPosition, initialVelocity, msPollInterval);
     }
 
     public Position getCoordinates() {
         return getPosition();
-
     }
 }
-
